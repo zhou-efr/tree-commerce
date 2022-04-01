41f88a2654e0cefa42ab6c7aedc884d46e313b7a
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-04-02 02:51:54 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1 className="text-xl text-amber-900">panda</h1>
-    </div>
-  );
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+  )
 }
-
-export default App;
