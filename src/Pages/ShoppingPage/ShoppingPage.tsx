115fe2056a6cd9c8fb0fc41fe348b2d7f2816f26
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-05-03 20:06:26 +0800

    shopp

--- src/Pages/ShoppingPage/ShoppingPage.tsx
+++ src/Pages/ShoppingPage/ShoppingPage.tsx
@@ -53,7 +53,7 @@ export const ShoppingPage:FC = () => {
     return (
     <div className={`ShoppingPage relative w-screen overflow-x-hidden`}>
         <div className={"z-10 absolute top-0 right-32 w-1/3 h-16 border-x-2 border-b-2 border-black bg-white flex justify-center font-light tracking-wider items-center uppercase text-3xl "}>{filterParameter || filter}</div>
-        <div className={`z-0 h-[88vh] flex w-full -mt-px overflow-x-hidden grid grid-cols-4`}>
+        <div className={`z-0 h-[88vh] flex w-full -mt-px overflow-x-hidden grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4`}>
             {filteredTrees?.map((tree, index) =>
                 <Item key={index} tree={tree}/>
             )}
