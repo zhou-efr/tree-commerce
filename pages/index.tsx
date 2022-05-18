c762ce0875301bd704dd0b6dce0b37960e7cef26
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-05-19 00:20:11 +0800

    next before migration

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Tree commerce</title>
+        <meta name="description" content="Where trees are models." />
+        <link rel="icon" href="/public/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
